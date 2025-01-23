8d9b72a218604cee264b0baf1c4997386fd51180
Iuri Paiva <iuripsilva27@gmail.com>
2025-01-23 02:33:15 -0300

    adição de features
    - adicionada uma loja para comportar os upgrades
    - melhora nas fontes e hitboxes dos botões

--- autokiller-navegador.service.worker.js
+++ autokiller-navegador.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1737594007|7444580194";
+const CACHE_VERSION = "1737610321|23758146431";
 const CACHE_PREFIX = "Autokiller-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "autokiller-navegador.offline.html";
