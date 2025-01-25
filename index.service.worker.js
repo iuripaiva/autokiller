9e41dc590e433f4bd4ac85675d61a5fa77e0f99d
Iuri Paiva <iuripsilva27@gmail.com>
2025-01-25 04:10:04 -0300

    atualização de melhorias
    - melhorias na UI da loja
    - correções de bugs com a captação do mouse

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,14 +3,14 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1737610656|24093939616";
+const CACHE_VERSION = "1737788956|13460671231";
 const CACHE_PREFIX = "Autokiller-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
-const OFFLINE_URL = "autokiller-navegador.offline.html";
+const OFFLINE_URL = "index.offline.html";
 // Files that will be cached on load.
-const CACHED_FILES = ["autokiller-navegador.html","autokiller-navegador.js","autokiller-navegador.offline.html","autokiller-navegador.icon.png","autokiller-navegador.apple-touch-icon.png"];
+const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png"];
 // Files that we might not want the user to preload, and will only be cached on first load.
-const CACHABLE_FILES = ["autokiller-navegador.wasm","autokiller-navegador.pck"];
+const CACHABLE_FILES = ["index.wasm","index.pck"];
 const FULL_CACHE = CACHED_FILES.concat(CACHABLE_FILES);
 
 self.addEventListener("install", (event) => {
